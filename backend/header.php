55a9ad69222e7622447a1d49013a4a5f5ab1df15
Kevin <kevin@na7kr.us>
2024-08-24 19:27:42 -0700

    ico added

--- backend/header.php
+++ backend/header.php
@@ -36,6 +36,8 @@ $submenu = array_filter($admin['submenu'], function($item) use ($isLoggedIn) {
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?php echo $title; ?></title>
     <link rel="stylesheet" href="/styles.css"> <!-- Update the path accordingly -->
+    <link rel="icon" href="/favicon.ico" type="image/x-icon">
+    <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon">
 </head>
 <body>
     <nav class="menu">
